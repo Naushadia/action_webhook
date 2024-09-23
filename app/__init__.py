70139b28022aabe10f78a47b4d4f93e5c0d47da9
Shadab <sadabia854318@gmail.com>
2024-09-23 07:13:18 +0530

    added homepage

--- app/__init__.py
+++ app/__init__.py
@@ -7,8 +7,12 @@ from app.webhook.routes import webhook
 def create_app():
 
     app = Flask(__name__)
-    
+
+    @app.route("/", methods=["GET"])
+    def index():
+        return "Welcome to our homepage!"
+
     # registering all the blueprints
     app.register_blueprint(webhook)
-    
+
     return app
