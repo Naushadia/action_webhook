98ddf4e5a891f7953eb40a4bbfe6293d070fdc5f
Shadab <sadabia854318@gmail.com>
2024-09-23 12:28:31 +0530

    updated data

--- app/webhook/routes.py
+++ app/webhook/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, json, request
+from flask import Blueprint, request
 from ..extensions import client
 
 webhook = Blueprint("Webhook", __name__, url_prefix="/webhook")
@@ -8,14 +8,37 @@ webhook = Blueprint("Webhook", __name__, url_prefix="/webhook")
 def receiver():
     collection = client.db["webhook_store"]
     data = request.get_json()
-    # extracted_data = {
-    #     "request_id": data["head_commit"]["id"],
-    #     "author": data["head_commit"]["author"]["name"],
-    #     "action": data["action"],
-    #     "from_branch": data["pull_request"]["head"]["ref"],
-    #     "to_branch": data["pull_request"]["base"]["ref"],
-    #     "timestamp": data["pull_request"]["created_at"],
-    # }
+    extracted_data = {
+        "request_id": None,
+        "author": None,
+        "action": None,
+        "from_branch": None,
+        "to_branch": None,
+        "timestamp": None,
+    }
 
-    collection.insert_one(data)
+    event = request.headers.get("X-GitHub-Event")
+
+    if event == "push":
+        extracted_data["request_id"] = data["head_commit"]["id"]
+        extracted_data["author"] = data["head_commit"]["author"]["name"]
+        extracted_data["action"] = "push"
+        extracted_data["from_branch"] = data["ref"].split("/")[-1]
+        extracted_data["timestamp"] = data["head_commit"]["timestamp"]
+
+    elif event == "pull_request":
+        action = data["action"]
+        extracted_data["request_id"] = data["pull_request"]["id"]
+        extracted_data["author"] = data["pull_request"]["user"]["login"]
+        extracted_data["action"] = action
+        extracted_data["from_branch"] = data["pull_request"]["head"]["ref"]
+        extracted_data["to_branch"] = data["pull_request"]["base"]["ref"]
+        extracted_data["timestamp"] = data["pull_request"]["created_at"]
+
+        if action == "closed" and data["pull_request"]["merged"]:
+            extracted_data["action"] = "merge"
+            extracted_data["timestamp"] = data["pull_request"]["merged_at"]
+
+    collection.insert_one(extracted_data)
+
     return {}, 200
