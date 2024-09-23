3e128afe6977d5e1037351bae470d54112c57b0b
Shadab <sadabia854318@gmail.com>
2024-09-23 14:47:24 +0530

    added cors

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,14 @@
 from flask import Flask
 import os
 from app.webhook.routes import webhook
+from flask_cors import CORS
 
 
 def create_app():
 
     template_dir = os.path.abspath("templates")
     app = Flask(__name__, template_folder=template_dir)
+    CORS(app)
 
     @app.route("/", methods=["GET"])
     def index():
