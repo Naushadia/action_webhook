a37313bf3c91aa7d595a67ca16fcda13861bb785
Shadab <sadabia854318@gmail.com>
2024-09-23 12:14:41 +0530

    updated data

--- app/webhook/routes.py
+++ app/webhook/routes.py
@@ -8,6 +8,14 @@ webhook = Blueprint("Webhook", __name__, url_prefix="/webhook")
 def receiver():
     collection = client.db["webhook_store"]
     data = request.get_json()
-    print(data)
-    collection.insert_one(data)
+    extracted_data = {
+        "request_id": data["head_commit"]["id"],
+        "author": data["head_commit"]["author"]["name"],
+        "action": data["action"],
+        "from_branch": data["pull_request"]["head"]["ref"],
+        "to_branch": data["pull_request"]["base"]["ref"],
+        "timestamp": data["pull_request"]["created_at"],
+    }
+
+    collection.insert_one(extracted_data)
     return {}, 200
